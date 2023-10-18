90d5981f419fbd2149564c7f135592d5117bd963
19zfl <19aleiya@gmail.com>
2023-10-18 16:30:42 +0800

    ***NineteenTools**

--- src/main/java/com/nineteentools/Main.java
+++ src/main/java/com/nineteentools/Main.java
@@ -1,7 +1,11 @@
 package com.nineteentools;
 
+import com.nineteentools.http.spider.SpiderTool;
+import org.jsoup.nodes.Document;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("******NineteenTools******");
+        Document documentElement = SpiderTool.getDocumentElement("https://wallhaven.cc/");
+        System.out.println(documentElement);
     }
 }
