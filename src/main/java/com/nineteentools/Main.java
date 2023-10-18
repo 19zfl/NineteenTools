837b570faf32e9ded25a96a21bb9f3e806a5aaed
19zfl <19aleiya@gmail.com>
2023-10-18 19:07:18 +0800

    ......

--- src/main/java/com/nineteentools/Main.java
+++ src/main/java/com/nineteentools/Main.java
@@ -1,11 +1,17 @@
 package com.nineteentools;
 
+import com.nineteentools.core.authCodeUtil.AuthCode;
+import com.nineteentools.core.authCodeUtil.constants.CharacterResources;
 import com.nineteentools.http.spider.SpiderTool;
 import org.jsoup.nodes.Document;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        Document documentElement = SpiderTool.getDocumentElement("https://wallhaven.cc/");
-        System.out.println(documentElement);
+        List<String> collect = Arrays.stream(CharacterResources.types).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
