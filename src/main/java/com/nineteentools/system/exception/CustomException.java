b7aceb1f52c6977187a83237c52ea554e8dfacc2
19zfl <19aleiya@gmail.com>
2023-10-18 16:28:12 +0800

    add CustomException.java

---
+++ src/main/java/com/nineteentools/system/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.nineteentools.system.exception;
+
+/**
+ * @author: 19zfl
+ * @description: 自定义异常
+ * @date 2023-10-17
+ */
+public class CustomException extends Exception {
+
+    public CustomException(String msg) {
+        super(msg);
+    }
+
+}
