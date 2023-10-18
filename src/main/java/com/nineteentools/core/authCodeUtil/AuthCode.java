ea63470d4fb082fe4840362f204cf43305f48e8d
19zfl <19aleiya@gmail.com>
2023-10-18 19:07:11 +0800

    add method[beta]

--- src/main/java/com/nineteentools/core/authCodeUtil/AuthCode.java
+++ src/main/java/com/nineteentools/core/authCodeUtil/AuthCode.java
@@ -1,9 +1,35 @@
 package com.nineteentools.core.authCodeUtil;
 
+import com.nineteentools.core.authCodeUtil.constants.CharacterResources;
+import com.nineteentools.core.authCodeUtil.utils.VerificationCodeUtil;
+import com.nineteentools.system.exception.CustomException;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author: 19zfl
- * @description:
+ * @description: 验证码工具（链式调用）
  * @date 2023-10-18
  */
 public class AuthCode {
+
+    public static String allCharWithoutNumber(Boolean flag) {
+            return flag ? VerificationCodeUtil.SixCharRandomVerificationCode() : VerificationCodeUtil.SixCharNumberRandomVerificationCode();
+    }
+
+    public static String verificationCode(String type, Integer bit) {
+
+        // 判断是否遵循规则
+        List<String> typeList = Arrays.stream(CharacterResources.types).collect(Collectors.toList());
+        if (!typeList.contains(type)) {
+
+        }
+
+        return null;
+    }
+
 }
